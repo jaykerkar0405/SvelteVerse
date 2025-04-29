c493d3d120d912577bc06fd5c75196236bf7bd5d
Jay Kerkar <bytewise0405@gmail.com>
2025-04-29 19:55:12 +0530

    feat: enhance login form with Google and GitHub login options and update links

--- src/lib/components/login-form.svelte
+++ src/lib/components/login-form.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
-	import { Button } from '$lib/components/ui/button/index.js';
-	import * as Card from '$lib/components/ui/card/index.js';
-	import { Input } from '$lib/components/ui/input/index.js';
-	import { Label } from '$lib/components/ui/label/index.js';
+	import { Github, Chrome } from 'lucide-svelte';
+	import * as Card from '$lib/components/ui/card';
+	import { Input } from '$lib/components/ui/input';
+	import { Label } from '$lib/components/ui/label';
+	import { Button } from '$lib/components/ui/button';
 </script>
 
 <Card.Root class="mx-auto max-w-sm">
@@ -19,16 +20,33 @@
 			<div class="grid gap-2">
 				<div class="flex items-center">
 					<Label for="password">Password</Label>
-					<a href="##" class="ml-auto inline-block text-sm underline"> Forgot your password? </a>
+					<a href="/" class="ml-auto inline-block text-sm underline"> Forgot your password? </a>
 				</div>
 				<Input id="password" type="password" required />
 			</div>
 			<Button type="submit" class="w-full">Login</Button>
-			<Button variant="outline" class="w-full">Login with Google</Button>
+
+			<div class="relative">
+				<div class="absolute inset-0 flex items-center">
+					<span class="w-full border-t"></span>
+				</div>
+				<div class="relative flex justify-center text-xs uppercase">
+					<span class="bg-card px-2 text-muted-foreground">or</span>
+				</div>
+			</div>
+
+			<Button variant="outline" class="w-full">
+				<Chrome class="mr-2 h-4 w-4" />
+				Login with Google
+			</Button>
+			<Button variant="outline" class="w-full">
+				<Github class="mr-2 h-4 w-4" />
+				Login with GitHub
+			</Button>
 		</div>
 		<div class="mt-4 text-center text-sm">
 			Don't have an account?
-			<a href="##" class="underline"> Sign up </a>
+			<a href="/" class="underline"> Sign up </a>
 		</div>
 	</Card.Content>
 </Card.Root>
