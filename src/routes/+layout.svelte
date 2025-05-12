91203204a738cccde94b3625288053a2feace8cc
Jay Kerkar <bytewise0405@gmail.com>
2025-05-12 15:30:26 +0530

    feat: update layout to handle user data and set authentication state

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
 	import '../app.css';
 	import { ModeWatcher } from 'mode-watcher';
+	import { setAuth } from '$lib/hooks/use-auth';
 	import { Toaster } from '$lib/components/ui/sonner';
 
-	let { children } = $props();
+	let { data, children } = $props<{ data: { user: any } }>();
+
+	setAuth(data.user);
 </script>
 
 <ModeWatcher />
