da8c7a3267232265ff93a98d11f3e047d5971d1f
Jay Kerkar <bytewise0405@gmail.com>
2025-05-16 19:08:43 +0530

    refactor: streamline service worker implementation and update Vite config for ES module support

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -1,58 +1,17 @@
-// /// <reference lib="esnext" />
-// /// <reference lib="webworker" />
-// /// <reference no-default-lib="true"/>
-// /// <reference types="@sveltejs/kit" />
+/// <reference lib="webworker" />
+declare const self: ServiceWorkerGlobalScope;
 
-// import { build, files, version } from '$service-worker';
+self.addEventListener('install', (event) => {
+	self.skipWaiting();
+});
 
-// const CACHE = `cache-${version}`;
-// const ASSETS = [...build, ...files];
-// const sw = self as unknown as ServiceWorkerGlobalScope;
+self.addEventListener('activate', (event) => {
+	event.waitUntil(
+		Promise.all([
+			self.clients.claim(),
+			caches.keys().then((keys) => Promise.all(keys.map((key) => caches.delete(key))))
+		])
+	);
+});
 
-// sw.addEventListener('install', (event) => {
-// 	event.waitUntil(
-// 		(async () => {
-// 			const cache = await caches.open(CACHE);
-// 			await cache.addAll(ASSETS);
-// 		})()
-// 	);
-// });
-
-// sw.addEventListener('activate', (event) => {
-// 	event.waitUntil(
-// 		(async () => {
-// 			const keys = await caches.keys();
-// 			await Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)));
-// 		})()
-// 	);
-// });
-
-// sw.addEventListener('fetch', (event) => {
-// 	if (event.request.method !== 'GET') return;
-// 	if (event.request.headers.has('range')) return;
-
-// 	event.respondWith(
-// 		(async () => {
-// 			const cache = await caches.open(CACHE);
-// 			const cachedResponse = await cache.match(event.request);
-
-// 			if (cachedResponse) {
-// 				return cachedResponse;
-// 			}
-
-// 			const networkResponse = await fetch(event.request);
-// 			if (networkResponse && networkResponse.status === 200 && networkResponse.type === 'basic') {
-// 				if (event.request.url.startsWith('http')) {
-// 					await cache.put(event.request, networkResponse.clone());
-// 				}
-// 			}
-// 			return networkResponse;
-// 		})()
-// 	);
-// });
-
-// sw.addEventListener('message', (event) => {
-// 	if (event.data && event.data.type === 'SKIP_WAITING') {
-// 		sw.skipWaiting();
-// 	}
-// });
+export {};
