b9face8ab0b9f3dba0ee8a5f77302342c0450e2b
Jay Kerkar <bytewise0405@gmail.com>
2025-05-16 19:09:50 +0530

    feat: add update sheet component for notifying users of available updates

---
+++ src/lib/components/update-sheet.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import * as Sheet from '$lib/components/ui/sheet';
+	import { applyUpdate } from '$lib/utils/update-handler';
+
+	let open = false;
+
+	onMount(() => {
+		window.addEventListener('show-update-sheet', () => {
+			open = true;
+		});
+	});
+
+	function handleUpdate() {
+		applyUpdate();
+		open = false;
+		window.location.reload();
+	}
+
+	function testUpdateSheet() {
+		window.dispatchEvent(new Event('show-update-sheet'));
+	}
+</script>
+
+<Sheet.Root bind:open>
+	<Sheet.Content side="bottom">
+		<Sheet.Header>
+			<Sheet.Title>Update Available</Sheet.Title>
+			<Sheet.Description>
+				A new version of the app is available. Would you like to update now?
+			</Sheet.Description>
+		</Sheet.Header>
+		<div class="mt-6 flex justify-end space-x-4">
+			<Sheet.Close class="rounded-md bg-secondary px-4 py-2 text-secondary-foreground">
+				Remind Me Later
+			</Sheet.Close>
+			<button
+				on:click={handleUpdate}
+				class="rounded-md bg-primary px-4 py-2 text-primary-foreground"
+			>
+				Update Now
+			</button>
+		</div>
+	</Sheet.Content>
+</Sheet.Root>
