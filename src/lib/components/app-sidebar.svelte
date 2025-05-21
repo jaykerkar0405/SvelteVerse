ca55c4ac5003981efe42b989e84e197146f7dda5
Jay Kerkar <bytewise0405@gmail.com>
2025-05-21 21:09:18 +0530

    Update app-sidebar to rename 'Audio' to 'Audio Room' and modify Peer to Peer item with a new URL structure. Wrap logo in a link to the dashboard for improved navigation.

--- src/lib/components/app-sidebar.svelte
+++ src/lib/components/app-sidebar.svelte
@@ -17,15 +17,11 @@
 			items: [
 				{
 					icon: Mic,
-					title: 'Audio',
+					title: 'Audio Room',
 					items: [
 						{
-							url: '',
-							title: 'peer 2 peer'
-						},
-						{
-							url: '',
-							title: 'multi peer'
+							title: 'Peer to Peer',
+							url: '/audio/peer-to-peer'
 						}
 					]
 				}
@@ -36,16 +32,18 @@
 
 <Sidebar.Root bind:ref {collapsible} {...restProps}>
 	<Sidebar.Header>
-		<Sidebar.MenuButton size="lg">
-			<div class="flex aspect-square size-8 items-center justify-center">
-				<img src="/logo.png" alt="Logo" class="rounded-lg" />
-			</div>
+		<a href="/dashboard">
+			<Sidebar.MenuButton size="lg">
+				<div class="flex aspect-square size-8 items-center justify-center">
+					<img src="/logo.png" alt="Logo" class="rounded-lg" />
+				</div>
 
-			<div class="ml-1 grid flex-1 text-left text-sm leading-tight">
-				<span class="truncate font-semibold">SvelteVerse</span>
-				<span class="truncate text-xs">A universe of SvelteKit</span>
-			</div>
-		</Sidebar.MenuButton>
+				<div class="ml-1 grid flex-1 text-left text-sm leading-tight">
+					<span class="truncate font-semibold">SvelteVerse</span>
+					<span class="truncate text-xs">A universe of SvelteKit</span>
+				</div>
+			</Sidebar.MenuButton>
+		</a>
 	</Sidebar.Header>
 
 	<Sidebar.Content>
